3a5553e30f06cf379e1d45516fcc9671b6680886
Abdurauf <tuychiev8951@gmail.com>
2023-02-08 18:53:33 +0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { User } from "./User";
+import { Company } from "./Company"
+
+// User
+const user = new User()
+
+console.log(user)
+
+//Company
+const company = new Company();
+
+console.log(company)
