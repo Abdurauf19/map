e551d68ed7cbaf02ef42261bc1ed1379f842d3d7
Abdurauf <tuychiev8951@gmail.com>
2023-02-09 20:10:06 +0500

    refactor: map zoom

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -10,7 +10,7 @@ export class CustomMap {
   private googleMap: google.maps.Map
   constructor(divId:string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
-      zoom: 1,
+      zoom: 2,
       center: {
         lat: 0,
         lng: 0
